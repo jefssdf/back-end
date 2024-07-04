306ff016594c7a0ba2d33cdc4e8437584ff416a5
Thiago da Silva Rangel <slipknothiago@gmail.com>
2024-07-04 12:04:57 -0300

    Fix GuidValidator

--- AgendaApi.Application/Shared/GlobalValidators/GuidValidator.cs
+++ AgendaApi.Application/Shared/GlobalValidators/GuidValidator.cs
@@ -1,6 +1,6 @@
 ﻿namespace AgendaApi.Application.Shared.GlobalValidators
 {
-    public sealed class GuidValidator
+    public static class GuidValidator
     {
         public static bool BeValid(Guid guid) =>
             Guid.TryParse(guid.ToString(), out _);
